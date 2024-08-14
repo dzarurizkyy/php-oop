e273bbe0693495405817247944d25ec7023990be
dzarurizkyy <dzarurizkybusiness@gmail.com>
2024-08-14 22:08:04 +0700

    finish material 6 : inheritance (2)

--- 005. Inheritance (2)/index.php
+++ 005. Inheritance (2)/index.php
@@ -36,24 +36,45 @@
 
     public function getInfo() {
       $price = number_format($this->price, 0, ",", ".");
+      $text  = "<b>Type</b>      : {$this->type} <br />";
+      $text .= "<b>Product</b>   : {$this->getLabel()} <br />";
+      $text .= "<b>Publisher</b> : {$this->publisher} <br />";
+      $text .= "<b>Price</b>     : Rp{$price} <br />";
+
+      return $text;
+    }
+  }
+
+  // Inheritance
+  class Book extends Product {
+    public function getInfo() {
+      $price = number_format($this->price, 0, ",", ".");
       $text  = "<b>Type</b>         : {$this->type} <br />";
       $text .= "<b>Product</b>      : {$this->getLabel()} <br />";
       $text .= "<b>Publisher</b>    : {$this->publisher} <br />";
       $text .= "<b>Price</b>        : Rp{$price} <br />";
+      $text .= "<b>Total Page</b>   : {$this->total_page} pages <br />";
 
-      if ($this->type === "Book") {
-        $text .= "<b>Total Page</b> : {$this->total_page} pages <br />";
-      } else {
-        $text .= "<b>Total Hour</b> : {$this->total_hour} hours <br />";
-      }
+      return $text;
+    }
+  }
+
+  class Game extends Product {
+    public function getInfo() {
+      $price = number_format($this->price, 0, ",", ".");
+      $text  = "<b>Type</b>         : {$this->type} <br />";
+      $text .= "<b>Product</b>      : {$this->getLabel()} <br />";
+      $text .= "<b>Publisher</b>    : {$this->publisher} <br />";
+      $text .= "<b>Price</b>        : Rp{$price} <br />";
+      $text .= "<b>Total Hour</b>   : {$this->total_hour} hours <br />";
 
       return $text;
     }
   }
 
   // Object
-  $product[0] = new Product("Atomic Habits", "James Clear", "Gramedia Pustaka Utama", 102400, 356, 0);
-  $product[1] = new Product("Only Up", "MoreMoto Games", "MoreMoto Games", 47499, 0, 3);
+  $product[0] = new Book("Atomic Habits", "James Clear", "Gramedia Pustaka Utama", 102400, 356, 0);
+  $product[1] = new Game("Only Up", "MoreMoto Games", "MoreMoto Games", 47499, 0, 3);
 
   // Print
   foreach($product as $index => $data) {
